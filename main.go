2bf131d0681b44a781646a82b47de78871c5d18d
Milkias Tonji <tongisttonji@gmail.com>
2023-06-27 10:44:31 +0300

    All structs have been added

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// import packages
 import (
 	"log"
 	"net/http"
@@ -7,6 +8,38 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// create stucks
+
+type Block struct {
+	Pos       int
+	Data      BookCheckout
+	TimesTamp string
+	Hash      string
+	PrevHash  string
+}
+
+type BookCheckout struct {
+	BookID       string `json:"book_id"`
+	User         string `json:"user"`
+	CheckoutDate string `json:"checkout_date"`
+	IsGenesis    bool   `json:"is_genesis"`
+}
+
+type Book struct {
+	ID            string `json:"id"`
+	Title         string `json:"title"`
+	Author        string `json:"author"`
+	PublishedDate string `json:"published_date"`
+	ISBN          string `json:"isbn"`
+}
+
+type BlockChain struct {
+	blocks []*Block
+}
+
+var BlockChain *BlockChain
+
+// define main func
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", getBlockChain).Methods("GET")
